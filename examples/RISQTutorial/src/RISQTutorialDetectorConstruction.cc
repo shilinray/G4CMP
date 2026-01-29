b603232714d0078e5db700bc74b85cb885a17c10
Shilin Ray <shilin@sdfiana007.sdf.slac.stanford.edu>
2026-01-28 21:49:09 -0800

    qpd done

--- examples/RISQTutorial/src/RISQTutorialDetectorConstruction.cc
+++ examples/RISQTutorial/src/RISQTutorialDetectorConstruction.cc
@@ -681,7 +681,7 @@ AttachPhononSensor(G4CMPSurfaceProperty *surfProp) {
 
   // Properties must be added to existing surface-property table
   auto sensorProp = surfProp->GetPhononMaterialPropertiesTablePointer();
-  sensorProp->AddConstProperty("filmAbsorption", 0);  //.795 
+  sensorProp->AddConstProperty("filmAbsorption", .795);  //.795 
   sensorProp->AddConstProperty("filmThickness", 100.*nm);
   sensorProp->AddConstProperty("gapEnergy", 173.715e-6*eV);
   sensorProp->AddConstProperty("lowQPLimit", 3.);
