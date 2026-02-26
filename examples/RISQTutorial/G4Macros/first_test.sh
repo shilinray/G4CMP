fb1982e1898d202e4e3504b3717663012ca07be7
Shilin Ray <shilin@sdfiana007.sdf.slac.stanford.edu>
2026-02-25 18:19:15 -0800

    jhgfd

--- examples/RISQTutorial/G4Macros/first_test.sh
+++ examples/RISQTutorial/G4Macros/first_test.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=roma
 #SBATCH --account=supercdms:default
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4096
 #SBATCH --time=0-02:00:00
 #SBATCH --gpus 0
