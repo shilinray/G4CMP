d5f88f0d03b12afe3fe791b00689b6b0bea65e04
Shilin Ray <shilin@sdfiana007.sdf.slac.stanford.edu>
2026-03-17 15:34:56 -0700

    hgf

--- examples/RISQTutorial/G4Macros/thickness.sh
+++ examples/RISQTutorial/G4Macros/thickness.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=roma
 #SBATCH --account=supercdms:default
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=21
 #SBATCH --mem-per-cpu=4096
 #SBATCH --time=0-02:00:00
 #SBATCH --gpus 0
@@ -27,15 +27,18 @@ fi
 Al_z="$1"
 Nb_z="$2"
 
+run_dir="./260317_run/SQUAT"
+tag="Al${Al_z}_Nb${Nb_z}"
+
 echo "Al thickness: $Al_z"
 echo "Nb thickness: $Nb_z"
 
-# creates output files names and macro file name
-fHits="/sdf/scratch/supercdms/sray/260317_run/Hits_Al${Al_z}_Nb${Nb_z}.txt"
-fPrimary="/sdf/scratch/supercdms/sray/260317_run/Primary_Al${Al_z}_Nb${Nb_z}.txt"
-mName="Run_Al${Al_z}_Nb${Nb_z}.mac"
+mkdir -p "$run_dir"
 
-# prints out a summary
+fHits="$run_dir/Hits_${tag}.txt"
+fPrimary="$run_dir/Primary_${tag}.txt"
+mName="$run_dir/Run_${tag}.mac"
+
 echo "Base Macro $BaseMacro"
 echo "Macro File $mName"
 echo "Hits File $fHits"
@@ -46,8 +49,8 @@ cp $BaseMacro $mName
 
 sed -i "s/filmThicknessAl 0/filmThicknessAl ${Al_z}/g" $mName
 sed -i "s/filmThicknessNb 0/filmThicknessNb ${Nb_z}/g" $mName
-sed -i "s|hitsFileName|hitsFileName '${fHits}'|g" $mName
-sed -i "s|primFileName|primFileName '${fPrimary}'|g" $mName
+sed -i "s|hitsFileName|hitsFileName ${fHits}|g" $mName
+sed -i "s|primFileName|primFileName ${fPrimary}|g" $mName
 
 apptainer exec /sdf/group/supercdms/software/releases/cdmsfull_V05-02.sif \
   /sdf/home/s/shilin/mycode/G4CMP/examples/RISQTutorial/RISQTutorial-build/RISQTutorial \
